9fe27ee1721b6fdee33503f8e2a0c068840eb342
woohdang <gvgmf16@gmail.com>
2024-10-26 17:29:44 -0300

    descomentada

--- importJson.py
+++ importJson.py
@@ -16,25 +16,25 @@ with open('/mnt/data/canchas_info.json', 'r') as file:
     fields_data = json.load(file)
 
 # Function to get latitude and longitude using Google Maps API
-# def get_coordinates(address):
-#   api_key = "Google_maps_api_key_XXXXXXXXXXXXXX"
-#    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
-#    params = {"address": address, "key": api_key}
-#    response = requests.get(base_url, params=params)
-#    result = response.json()
-#    if result['status'] == 'OK':
-#        location = result['results'][0]['geometry']['location']
-#        return location['lat'], location['lng']
-#    else:
-#        return None, None
+def get_coordinates(address):
+    api_key = "Google_maps_api_key_XXXXXXXXXXXXXX"
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {"address": address, "key": api_key}
+    response = requests.get(base_url, params=params)
+    result = response.json()
+    if result['status'] == 'OK':
+        location = result['results'][0]['geometry']['location']
+        return location['lat'], location['lng']
+    else:
+        return None, None
 
 # Connect to MySQL database
-# connection = mysql.connector.connect(
-#    host='localhost',  # Change this to your database address
-#    user='root',  # Change this to your MySQL user
-#    password='password',  # Change this to your MySQL password
-#    database='picadosYa'
-
+connection = mysql.connector.connect(
+    host='localhost',  # Change this to your database address
+    user='root',  # Change this to your MySQL user
+    password='password',  # Change this to your MySQL password
+    database='picadosYa'
+)
 cursor = connection.cursor()
 
 # Insert data into "fields" table
@@ -52,10 +52,10 @@ for field in fields_data:
     image_urls = field.get('image_urls', [])  # List of image URLs
 
     # Get latitude and longitude from address using Google Maps API
-    # latitude, longitude = get_coordinates(address)
-    # if latitude is None or longitude is None:
-    #    print(f"Unable to get coordinates for address: {address}")
-    #    continue
+    latitude, longitude = get_coordinates(address)
+    if latitude is None or longitude is None:
+        print(f"Unable to get coordinates for address: {address}")
+        continue
 
     # Insert data into "fields" table
     insert_query = (
@@ -74,7 +74,7 @@ for field in fields_data:
         cursor.execute(insert_photo_query, (field_id, url))
 
     # Pause to avoid exceeding Google Maps API quota
-    #time.sleep(1)
+    time.sleep(1)
 
 # Commit changes and close connection
 connection.commit()
