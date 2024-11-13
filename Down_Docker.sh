23524b2f6e93d2eab96524789676ef6d70a12242
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-11-12 21:38:37 -0300

    cambios en configuracion para levantar la app y se agrega cenapp al proyecto

--- Down_Docker.sh
+++ Down_Docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker compose -f docker-compose.prod.yml down
+sudo docker compose -f docker-compose.prod.yml down
 
-docker image rm picadosya-app-frontend picadosya-app-backend
+sudo docker image rm picadosya-app-frontend picadosya-app-backend
 
-docker volume rm picadosya-app_mysql_volume
+sudo docker volume rm picadosya-app_mysql_volume
