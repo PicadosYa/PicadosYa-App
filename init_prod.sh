41aab4c0a7ee8969a2a3c6665133b2babe486af7
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-11-19 21:39:34 -0300

    agregando script para produccion

--- init_prod.sh
+++ init_prod.sh
@@ -6,6 +6,8 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
+git submodule update --init --recursive
+
 # Obtener la nueva IP desde el parámetro
 NEW_IP="$1"
 
@@ -36,8 +38,37 @@ else
   echo "Error: $VITE_FILE no encontrado."
 fi
 
+cp .env.prod .env && cp Backend/.env.template Backend/.env
+
+# Ruta del archivo .env
+ENV_FILE="Backend/.env"
+
+# Verificar si el archivo .env existe
+if [[ ! -f "$ENV_FILE" ]]; then
+  echo "Error: El archivo $ENV_FILE no existe."
+  exit 1
+fi
+
+# Modificar APP_BASE_URL
+sed -i "s|http://localhost:8080|http://$NEW_IP:8080|g" "$ENV_FILE"
+echo "APP_BASE_URL actualizado a http://$NEW_IP:8080."
+
+# Modificar DB_HOST, DB_PORT y DB_USER
+sed -i "s|DB_HOST=127.0.0.1|DB_HOST=mysql|g" "$ENV_FILE"
+sed -i "s|DB_PORT=3307|DB_PORT=3306|g" "$ENV_FILE"
+sed -i "s|DB_USER=root|DB_USER=picadosya|g" "$ENV_FILE"
+echo "DB_HOST, DB_PORT y DB_USER actualizados."
+
 # Verificación final
 echo "Modificaciones completadas. Verifique los archivos para confirmar los cambios:"
 echo "- $GLOBAL_FILE"
 echo "- $API_FILE"
 echo "- $VITE_FILE"
+
+echo "Modificaciones completadas en el archivo $ENV_FILE. Los cambios son:"
+echo "- APP_BASE_URL: http://$NEW_IP:8080"
+echo "- DB_HOST: mysql"
+echo "- DB_PORT: 3306"
+echo "- DB_USER: picadosya"
+
+# sudo docker compose -f docker-compose.prod.yml up -d
