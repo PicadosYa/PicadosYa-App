e300999ffbe2c2e6d34f04702fb46c4f04e30ba1
Deivid González <bienvenidoexperto@gmail.com>
2024-11-23 20:26:25 -0300

    feat: Reservation Modal

--- migrations/000014_fix_create_reservation_procedure.up.sql
+++ migrations/000014_fix_create_reservation_procedure.up.sql
@@ -5,11 +5,10 @@ CREATE PROCEDURE InsertReservation(
     IN p_user_id INT,
     IN p_date DATE,
     IN p_start_time TIME,
-    IN p_end_time TIME,
-    IN p_status ENUM('reserved', 'canceled', 'completed')
-)
+    IN p_end_time TIME
+  )
 BEGIN
-    INSERT INTO reservations (field_id, user_id, date, start_time, end_time, status)
-    VALUES (p_field_id, p_user_id, p_date, p_start_time, p_end_time, p_status);
+    INSERT INTO reservations (field_id, user_id, date, start_time, end_time)
+    VALUES (p_field_id, p_user_id, p_date, p_start_time, p_end_time);
 END
 
