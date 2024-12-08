91272f0723fade230cad92ce6bcf1b7689fb7010
Deivid González <bienvenidoexperto@gmail.com>
2024-12-08 19:55:46 -0300

    feat: add test data

---
+++ migrations/000023_field-owner-assignment.sql.up.sql
@@ -0,0 +1,665 @@
+-- Asignación de propietarios a canchas
+
+UPDATE fields 
+SET user_id = 653 
+WHERE id = 1;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 653;
+
+UPDATE fields 
+SET user_id = 768 
+WHERE id = 2;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 768;
+
+UPDATE fields 
+SET user_id = 548 
+WHERE id = 3;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 548;
+
+UPDATE fields 
+SET user_id = 337 
+WHERE id = 4;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 337;
+
+UPDATE fields 
+SET user_id = 414 
+WHERE id = 5;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 414;
+
+UPDATE fields 
+SET user_id = 303 
+WHERE id = 6;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 303;
+
+UPDATE fields 
+SET user_id = 397 
+WHERE id = 7;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 397;
+
+UPDATE fields 
+SET user_id = 629 
+WHERE id = 8;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 629;
+
+UPDATE fields 
+SET user_id = 239 
+WHERE id = 9;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 239;
+
+UPDATE fields 
+SET user_id = 324 
+WHERE id = 10;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 324;
+
+UPDATE fields 
+SET user_id = 173 
+WHERE id = 11;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 173;
+
+UPDATE fields 
+SET user_id = 981 
+WHERE id = 12;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 981;
+
+UPDATE fields 
+SET user_id = 830 
+WHERE id = 13;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 830;
+
+UPDATE fields 
+SET user_id = 49 
+WHERE id = 14;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 49;
+
+UPDATE fields 
+SET user_id = 452 
+WHERE id = 15;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 452;
+
+UPDATE fields 
+SET user_id = 244 
+WHERE id = 16;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 244;
+
+UPDATE fields 
+SET user_id = 966 
+WHERE id = 17;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 966;
+
+UPDATE fields 
+SET user_id = 203 
+WHERE id = 18;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 203;
+
+UPDATE fields 
+SET user_id = 222 
+WHERE id = 19;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 222;
+
+UPDATE fields 
+SET user_id = 95 
+WHERE id = 20;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 95;
+
+UPDATE fields 
+SET user_id = 587 
+WHERE id = 21;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 587;
+
+UPDATE fields 
+SET user_id = 211 
+WHERE id = 22;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 211;
+
+UPDATE fields 
+SET user_id = 639 
+WHERE id = 23;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 639;
+
+UPDATE fields 
+SET user_id = 835 
+WHERE id = 24;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 835;
+
+UPDATE fields 
+SET user_id = 964 
+WHERE id = 25;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 964;
+
+UPDATE fields 
+SET user_id = 976 
+WHERE id = 26;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 976;
+
+UPDATE fields 
+SET user_id = 111 
+WHERE id = 27;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 111;
+
+UPDATE fields 
+SET user_id = 457 
+WHERE id = 28;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 457;
+
+UPDATE fields 
+SET user_id = 295 
+WHERE id = 29;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 295;
+
+UPDATE fields 
+SET user_id = 209 
+WHERE id = 30;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 209;
+
+UPDATE fields 
+SET user_id = 219 
+WHERE id = 31;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 219;
+
+UPDATE fields 
+SET user_id = 875 
+WHERE id = 32;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 875;
+
+UPDATE fields 
+SET user_id = 620 
+WHERE id = 33;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 620;
+
+UPDATE fields 
+SET user_id = 402 
+WHERE id = 34;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 402;
+
+UPDATE fields 
+SET user_id = 550 
+WHERE id = 35;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 550;
+
+UPDATE fields 
+SET user_id = 766 
+WHERE id = 36;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 766;
+
+UPDATE fields 
+SET user_id = 734 
+WHERE id = 37;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 734;
+
+UPDATE fields 
+SET user_id = 532 
+WHERE id = 38;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 532;
+
+UPDATE fields 
+SET user_id = 751 
+WHERE id = 39;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 751;
+
+UPDATE fields 
+SET user_id = 730 
+WHERE id = 40;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 730;
+
+UPDATE fields 
+SET user_id = 998 
+WHERE id = 41;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 998;
+
+UPDATE fields 
+SET user_id = 305 
+WHERE id = 42;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 305;
+
+UPDATE fields 
+SET user_id = 293 
+WHERE id = 43;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 293;
+
+UPDATE fields 
+SET user_id = 648 
+WHERE id = 44;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 648;
+
+UPDATE fields 
+SET user_id = 177 
+WHERE id = 45;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 177;
+
+UPDATE fields 
+SET user_id = 205 
+WHERE id = 46;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 205;
+
+UPDATE fields 
+SET user_id = 566 
+WHERE id = 47;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 566;
+
+UPDATE fields 
+SET user_id = 552 
+WHERE id = 48;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 552;
+
+UPDATE fields 
+SET user_id = 698 
+WHERE id = 49;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 698;
+
+UPDATE fields 
+SET user_id = 4 
+WHERE id = 50;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 4;
+
+UPDATE fields 
+SET user_id = 54 
+WHERE id = 51;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 54;
+
+UPDATE fields 
+SET user_id = 960 
+WHERE id = 52;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 960;
+
+UPDATE fields 
+SET user_id = 987 
+WHERE id = 53;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 987;
+
+UPDATE fields 
+SET user_id = 543 
+WHERE id = 54;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 543;
+
+UPDATE fields 
+SET user_id = 753 
+WHERE id = 55;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 753;
+
+UPDATE fields 
+SET user_id = 178 
+WHERE id = 56;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 178;
+
+UPDATE fields 
+SET user_id = 950 
+WHERE id = 57;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 950;
+
+UPDATE fields 
+SET user_id = 319 
+WHERE id = 58;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 319;
+
+UPDATE fields 
+SET user_id = 838 
+WHERE id = 59;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 838;
+
+UPDATE fields 
+SET user_id = 562 
+WHERE id = 60;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 562;
+
+UPDATE fields 
+SET user_id = 831 
+WHERE id = 61;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 831;
+
+UPDATE fields 
+SET user_id = 939 
+WHERE id = 62;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 939;
+
+UPDATE fields 
+SET user_id = 721 
+WHERE id = 63;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 721;
+
+UPDATE fields 
+SET user_id = 654 
+WHERE id = 64;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 654;
+
+UPDATE fields 
+SET user_id = 725 
+WHERE id = 65;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 725;
+
+UPDATE fields 
+SET user_id = 41 
+WHERE id = 66;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 41;
+
+UPDATE fields 
+SET user_id = 440 
+WHERE id = 67;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 440;
+
+UPDATE fields 
+SET user_id = 991 
+WHERE id = 68;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 991;
+
+UPDATE fields 
+SET user_id = 525 
+WHERE id = 70;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 525;
+
+UPDATE fields 
+SET user_id = 667 
+WHERE id = 71;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 667;
+
+UPDATE fields 
+SET user_id = 840 
+WHERE id = 72;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 840;
+
+UPDATE fields 
+SET user_id = 267 
+WHERE id = 73;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 267;
+
+UPDATE fields 
+SET user_id = 595 
+WHERE id = 74;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 595;
+
+UPDATE fields 
+SET user_id = 259 
+WHERE id = 75;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 259;
+
+UPDATE fields 
+SET user_id = 201 
+WHERE id = 76;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 201;
+
+UPDATE fields 
+SET user_id = 801 
+WHERE id = 77;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 801;
+
+UPDATE fields 
+SET user_id = 278 
+WHERE id = 78;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 278;
+
+UPDATE fields 
+SET user_id = 821 
+WHERE id = 79;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 821;
+
+UPDATE fields 
+SET user_id = 69 
+WHERE id = 80;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 69;
+
+UPDATE fields 
+SET user_id = 464 
+WHERE id = 81;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 464;
+
+UPDATE fields 
+SET user_id = 262 
+WHERE id = 82;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 262;
+
+UPDATE fields 
+SET user_id = 561 
+WHERE id = 83;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 561;
+
+UPDATE fields 
+SET user_id = 925 
+WHERE id = 84;
+
+UPDATE users 
+SET role = 'field' 
+WHERE id = 925;
